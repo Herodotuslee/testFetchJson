166f1c529b07c20859cef1a770d93a49e17f02b2
Albert Lee <herodotuslee@gmail.com>
2022-09-27 07:48:32 -0700

    s

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// import data from './data.json';
+// console.log('xxx',data);
+console.log('test!!')
+// import data from './data.json' assert {type: 'json'};
+// console.log('???',data);
+// fetch('./data.json')
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
