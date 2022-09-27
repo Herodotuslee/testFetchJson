d44d1da7f22ad83129ce3a53b7a743cd22cc7c47
Albert Lee <herodotuslee@gmail.com>
2022-09-27 07:55:08 -0700

    clean code

--- index.js
+++
@@ -1,9 +0,0 @@
-
-// import data from './data.json';
-// console.log('xxx',data);
-console.log('test!!')
-// import data from './data.json' assert {type: 'json'};
-// console.log('???',data);
-// fetch('./data.json')
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
